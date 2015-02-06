cf2e6bcb4a6985c62a84b786139b1671815a4bb4
crosbourne <neilcrosbourne@hotmail.com>
2015-02-06 07:37:35 +0000

    function added to close profile when you move cursor

--- js/main.js
+++ js/main.js
@@ -43,91 +43,148 @@ window.onload = function() {
   $('#neil-portrait').mouseenter(function() {
     close_all_but('#neil-profile');
   });
+  $('#neil-portrait').mouseleave(function() {
+    close('#neil-profile');
+  });
   
 
   $('#luke-portrait').mouseenter(function() {
     close_all_but('#luke-profile');
+  });
+  $('#luke-portrait').mouseleave(function() {
+    close('#luke-profile');
   });
  
 
   $('#joel-portrait').mouseenter(function() {
     close_all_but('#joel-profile');
   });
+  $('#joel-portrait').mouseleave(function() {
+    close('#joel-profile');
+  });
   
 
   $('#sadiq-portrait').mouseenter(function() {
     close_all_but('#sadiq-profile');
+  });
+  $('#sadiq-portrait').mouseleave(function() {
+    close('#sadiq-profile');
   });
  
 
   $('#bruno-portrait').mouseenter(function() {
     close_all_but('#bruno-profile');
   });
+  $('#bruno-portrait').mouseleave(function() {
+    close('#bruno-profile');
+  });
  
 
   $('#mathieu-portrait').mouseenter(function() {
     close_all_but('#mathieu-profile');
+  });
+  $('#mathieu-portrait').mouseleave(function() {
+    close('#mathieu-profile');
   });
   
 
   $('#andres-portrait').mouseenter(function() {
     close_all_but('#andres-profile');
   });
+  $('#andres-portrait').mouseleave(function() {
+    close('#andres-profile');
+  });
   
   $('#sophie-portrait').mouseenter(function() {
     close_all_but('#sophie-profile');
+  });
+  $('#sophie-portrait').mouseleave(function() {
+    close('#sophie-profile');
   });
  
 
   $('#nina-portrait').mouseenter(function() {
     close_all_but('#nina-profile');
   });
+  $('#nina-portrait').mouseleave(function() {
+    close('#nina-profile');
+  });
  
 
   $('#lisa-portrait').mouseenter(function() {
     close_all_but('#lisa-profile');
   });
+  $('#lisa-portrait').mouseleave(function() {
+    close('#lisa-profile');
+  });
  
   $('#daniel-portrait').mouseenter(function() {
     close_all_but('#daniel-profile');
+  });
+  $('#daniel-portrait').mouseleave(function() {
+    close('#daniel-profile');
   });
   
 
   $('#sebastian-portrait').mouseenter(function() {
     close_all_but('#sebastian-profile');
   });
+  $('#sebastian-portrait').mouseleave(function() {
+    close('#sebastian-profile');
+  });
   
 
   $('#tony-portrait').mouseenter(function() {
     close_all_but('#tony-profile');
+  });
+  $('#tony-portrait').mouseleave(function() {
+    close('#tony-profile');
   });
  
 
   $('#julia-portrait').mouseenter(function() {
     close_all_but('#julia-profile');
   });
+  $('#julia-portrait').mouseleave(function() {
+    close('#julia-profile');
+  });
   
 
   $('#ellie-portrait').mouseenter(function() {
     close_all_but('#ellie-profile');
   });
+  $('#ellie-portrait').mouseleave(function() {
+    close('#ellie-profile');
+  });
 
   $('#rabia-portrait').mouseenter(function() {
     close_all_but('#rabia-profile');
   });
+  $('#rabia-portrait').mouseleave(function() {
+    close('#rabia-profile');
+  });
  
  $('#alex-portrait').mouseenter(function() {
    close_all_but('#alex-profile');
+ });
+ $('#alex-portrait').mouseleave(function() {
+   close('#alex-profile');
  });
  
 
  $('#joanna-portrait').mouseenter(function() {
    close_all_but('#joanna-profile');
  });
+ $('#joanna-portrait').mouseleave(function() {
+   close('#joanna-profile');
+ });
  
 
  $('#michael-portrait').mouseenter(function() {
    close_all_but('#michael-profile');
+ });
+ $('#michael-portrait').mouseleave(function() {
+   close('#michael-profile');
  });
 
  $('#jarkyn-portrait').mouseenter(function() {
